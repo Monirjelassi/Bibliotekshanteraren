ad5cc7ee44b15b7cd0010a1514bfae0ac0fec385
MonirJelassi <monirjelassi99@gmail.com>
2026-09-18 20:35:39 +0200

    cleaned up main by moving menu into a method

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,48 +2,10 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        boolean running = true;
 
-        while (running) {
-            System.out.println("1. Add book");
-            System.out.println("2. Register member");
-            System.out.println("3. Loan book");
-            System.out.println("4. Return book");
-            System.out.println("5. Search book");
-            System.out.println("6. Show all books");
-            System.out.println("7. Quit");
-            System.out.print("Choose an option: ");
-            int choice = scanner.nextInt();
-            scanner.nextLine();
-            switch (choice) {
-                case 1:
-                    System.out.println("Enter book name: ");
-                    break;
-                    case 2:
-                        System.out.println("Enter member name: ");
-                        break;
-                        case 3:
-                            System.out.println("book ID: ");
-                            break;
-                            case 4:
-                                System.out.println("book ID: ");
-                                break;
-                                case 5:
-                                    System.out.println("Search book ID: ");
-                                    break;
-                                    case 6:
-                                        //showing all books
-                                        break;
-                                        case 7:
-                                            System.out.println("Shutting down...");
-                                            running = false;
-                                            break;
-                                            default:
-                                                System.out.println("Invalid choice");
-            }
-        }
-        scanner.close();
+        Menu menu = new Menu();
+
+        menu.startMenu();
 
     }
 }
