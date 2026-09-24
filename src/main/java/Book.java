e72120c32300957c9a82a0c224505e655628cb4f
MonirJelassi <monirjelassi99@gmail.com>
2026-09-24 15:48:50 +0200

    Shorten Book isbn output with override toString
    done

--- src/main/java/Book.java
+++ src/main/java/Book.java
@@ -1,4 +1,9 @@
 import java.util.UUID;
 
 public record Book(UUID isbn, String title, String author) {
+
+    @Override
+    public String toString() {
+        return title + " by " + author + " [" + isbn.toString().substring(0, 8) + "]";
+    }
 }
